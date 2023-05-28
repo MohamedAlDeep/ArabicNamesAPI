be900ffce4f5c31953dcef56db111e266433aa6a
ICE988 <mohamedsamir22446688@gmail.com>
2023-05-28 17:46:53 +0300

    added new code

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+
+// Importing
 const express = require('express')
 const app = express()
 const fs = require('file-system');
+const bodyParser = require('body-parser')
 
+// Server port
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+// Some settings
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+
+// Routers
+app.post('/add', (req, res) => {
+  let name = req.body.name
+
+  app.get('/data', (req, res) => {
+    res.send(name)
+  })
+
+  res.redirect('/data')
 })
 
+// Server listening
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
