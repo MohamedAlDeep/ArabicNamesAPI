998a752ab4f253fce5e7860605dcfbc74e19c70f
ICE988 <mohamedsamir22446688@gmail.com>
2023-06-05 17:00:22 +0300

    new changes

--- server.js
+++ server.js
@@ -21,26 +21,30 @@ app.use(express.urlencoded())
 
 // Code
 
-app.post('/add', (req, res) => {
+app.post('/api/add', (req, res) => {
   const name = req.body.name
   let allObj = [
     {...Uname},
     {name: name}
   ]
+  Object.values(allObj).forEach(val => {
+    console.log(val.name);
+  });
+
   let data = JSON.stringify(allObj);
   fs.writeFileSync('names.json', data);
-  res.redirect('/data')
+  res.redirect('/api/data')
 })
 
-app.get('/data', (req, res) => {
+app.get('/api/data', (req, res) => {
       fs.readFile('names.json', 'utf8', (error, data) => {
         let newData = JSON.parse(data)
         res.json(newData)
       })
 })
 
+
 // Server listening
 app.listen(port, () => {
-
   console.log(`App listening on port ${port}`)
 })
